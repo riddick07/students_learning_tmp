fd316676c8c8e2111f6433e13770388e063790c8
ABEUTLER <alexander.beutler@allgeier-es.com>
2019-10-15 11:34:52 +0200

    Unified visibility zones. All variables now private and Getter/Setters are set up.

--- src/com/company/InsuranceData.java
+++ src/com/company/InsuranceData.java
@@ -1,12 +1,67 @@
 package com.company;
 
 public class InsuranceData {
-    boolean hasInsurance;
-    String company;
-    String expiry;
-    String contact;
-    String region;
-    int clazz;
-    double contribution;
+    private boolean hasInsurance;
+    private String company;
+    private String expiry;
+    private String contact;
+    private String region;
+    private int clazz;
+    private double contribution;
 
+    public boolean isHasInsurance() {
+        return hasInsurance;
+    }
+
+    public void setHasInsurance(boolean hasInsurance) {
+        this.hasInsurance = hasInsurance;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getExpiry() {
+        return expiry;
+    }
+
+    public void setExpiry(String expiry) {
+        this.expiry = expiry;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public int getClazz() {
+        return clazz;
+    }
+
+    public void setClazz(int clazz) {
+        this.clazz = clazz;
+    }
+
+    public double getContribution() {
+        return contribution;
+    }
+
+    public void setContribution(double contribution) {
+        this.contribution = contribution;
+    }
 }
