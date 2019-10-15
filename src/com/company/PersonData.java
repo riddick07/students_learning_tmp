c39ab4a9f2f6c529264d658b4b900de2b639bb17
MTREMMEL <moritz.tremmel@gmail.com>
2019-10-15 11:44:32 +0200

    public to private; getters & setters

--- src/com/company/PersonData.java
+++ src/com/company/PersonData.java
@@ -11,5 +11,67 @@ public class PersonData {
     private int children;
     private boolean isMarried;
 
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public void setChildren(int children) {
+        this.children = children;
+    }
+
+    public boolean isMarried() {
+        return isMarried;
+    }
+
+    public void setMarried(boolean married) {
+        isMarried = married;
+    }
 }
--- src/com/company/PriceData.java
+++ src/com/company/PriceData.java
@@ -8,4 +8,43 @@ public class PriceData {
     private int introduction;
     private String code;
 
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public int getCurrencyNumber() {
+        return currencyNumber;
+    }
+
+    public void setCurrencyNumber(int currencyNumber) {
+        this.currencyNumber = currencyNumber;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public int getIntroduction() {
+        return introduction;
+    }
+
+    public void setIntroduction(int introduction) {
+        this.introduction = introduction;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
 }
