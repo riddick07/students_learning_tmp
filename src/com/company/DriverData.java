2a0429f150ddd0800dd8602bfc2ada7a9fa3ba6f
ABEUTLER <alexander.beutler@allgeier-es.com>
2019-10-15 11:52:14 +0200

    Unified visibility zones. All variables now private and Getter/Setters are set up.

--- src/com/company/DriverData.java
+++ src/com/company/DriverData.java
@@ -1,29 +1,16 @@
 package com.company;
 
 public class DriverData {
-    private String surname;
-    private String givenName;
+    private PersonData driver;
     private String birthday;
-    private String birthplace;
-    private String residence;
     private int felonyCharges;         //Flensburg
-    private int age;
-    private boolean isMarried;
 
-    public String getSurname() {
-        return surname;
+    public PersonData getDriver() {
+        return driver;
     }
 
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getGivenName() {
-        return givenName;
-    }
-
-    public void setGivenName(String givenName) {
-        this.givenName = givenName;
+    public void setDriver(PersonData driver) {
+        this.driver = driver;
     }
 
     public String getBirthday() {
@@ -34,43 +21,11 @@ public class DriverData {
         this.birthday = birthday;
     }
 
-    public String getBirthplace() {
-        return birthplace;
-    }
-
-    public void setBirthplace(String birthplace) {
-        this.birthplace = birthplace;
-    }
-
-    public String getResidence() {
-        return residence;
-    }
-
-    public void setResidence(String residence) {
-        this.residence = residence;
-    }
-
     public int getFelonyCharges() {
         return felonyCharges;
     }
 
     public void setFelonyCharges(int felonyCharges) {
         this.felonyCharges = felonyCharges;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public boolean isMarried() {
-        return isMarried;
-    }
-
-    public void setMarried(boolean married) {
-        isMarried = married;
     }
 }
