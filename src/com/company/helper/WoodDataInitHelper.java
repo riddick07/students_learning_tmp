4d7e3950b774658b62cf6d26714a75934d43f464
DRIEPL <dominik.riepl@allgeier-es.com>
2019-10-16 10:56:16 +0200

    Wood Data Lists added

--- src/com/company/helper/WoodDataInitHelper.java
+++ src/com/company/helper/WoodDataInitHelper.java
@@ -9,50 +9,76 @@ import java.util.List;
 
 public class WoodDataInitHelper {
 
-    public static ForestData init(){
-        ForestData forestData = new ForestData();
-        forestData.setLocation(getLocationData());
-        forestData.setTrees(getTreesData());
-        return forestData;
-    }
-
-    public static List<TreeData> getTreesData() {
+    public static List<ForestData> init() {
+        List<ForestData> forests = new ArrayList<>();
         List<TreeData> trees = new ArrayList<>();
 
-        TreeData treeData = getTreeData();
-        TreeData treeDataTest = getTreeDataTest();
-        trees.add(treeData);
-        trees.add(treeDataTest);
+        ForestData forestData = new ForestData();
 
-        return trees;
-    }
+        TreeData treeData1 = new TreeData();
+        treeData1.setAgeInDays(125330);
+        treeData1.setColor("blue");
+        treeData1.setHeight(333);
+        treeData1.setOrigin("moon");
+        treeData1.setType("sometree");
 
-    public static TreeData getTreeData() {
-        TreeData treeData = new TreeData();
-        treeData.setAgeInDays(1250);
-        treeData.setColor("green");
-        treeData.setHeight(35);
-        treeData.setProduce("apple");
-        treeData.setOrigin("germany");
-        treeData.setType("apple tree");
-        return treeData;
-    }
 
-    public static TreeData getTreeDataTest() {
-        TreeData treeData = new TreeData();
-        treeData.setAgeInDays(3232);
-        treeData.setColor("green");
-        treeData.setHeight(21);
-        treeData.setOrigin("germany");
-        treeData.setType("cherry tree");
-        return treeData;
-    }
+        TreeData treeData2 = new TreeData();
+        treeData2.setAgeInDays(3232);
+        treeData2.setColor("Yellow");
+        treeData2.setHeight(2112122);
+        treeData2.setOrigin("China");
+        treeData2.setType("yellow tree");
 
-    public static LocationsData getLocationData() {
-        LocationsData locationsData = new LocationsData();
-        locationsData.setName("Freiburg");
-        locationsData.setLatitude(47.9);
-        locationsData.setLongitude(7.8);
-        return locationsData;
+        trees.add(treeData1);
+        trees.add(treeData2);
+
+
+        LocationsData locationsData1 = new LocationsData();
+        locationsData1.setName("USA");
+        locationsData1.setLatitude(777.9);
+        locationsData1.setLongitude(44.8);
+
+        forestData.setTrees(trees);
+        forestData.setLocation(locationsData1);
+        forests.add(forestData);
+
+
+        //---------------------------------------
+
+        List<TreeData> trees2 = new ArrayList<>();
+
+        ForestData forestData2 = new ForestData();
+
+        TreeData treeData12 = new TreeData();
+        treeData12.setAgeInDays(1250);
+        treeData12.setColor("green");
+        treeData12.setHeight(35);
+        treeData12.setProduce("apple");
+        treeData12.setOrigin("germany");
+        treeData12.setType("apple tree");
+
+
+        TreeData treeData22 = new TreeData();
+        treeData22.setAgeInDays(3232);
+        treeData22.setColor("green");
+        treeData22.setHeight(21);
+        treeData22.setOrigin("germany");
+        treeData22.setType("cherry tree");
+
+        trees2.add(treeData22);
+        trees2.add(treeData12);
+
+
+        LocationsData locationsData12 = new LocationsData();
+        locationsData12.setName("Kronberg");
+        locationsData12.setLatitude(50);
+        locationsData12.setLongitude(60);
+
+        forestData2.setTrees(trees2);
+        forestData2.setLocation(locationsData12);
+        forests.add(forestData);
+
+        return forests;
     }
 }
