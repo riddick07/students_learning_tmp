695db8271be00b93b29fcb31785b072cf8bf2b71
MTREMMEL <moritz.tremmel@gmail.com>
2019-10-16 10:57:13 +0200

    added *DataInitHelper Building

--- src/com/company/helper/BuildingDataInitHelper.java
+++ src/com/company/helper/BuildingDataInitHelper.java
@@ -3,9 +3,14 @@ package com.company.helper;
 import com.company.data.BuildingData;
 import com.company.data.PriceData;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BuildingDataInitHelper {
 
-    public static BuildingData init(){
+    public static List<BuildingData> init(){
+
+        List<BuildingData> list = new ArrayList<>();
 
         BuildingData buildingData = new BuildingData();
         buildingData.setCity("Frankfurt");
@@ -28,7 +33,32 @@ public class BuildingDataInitHelper {
 
         buildingData.setCost(priceData);
 
-        return buildingData;
+        list.add(buildingData);
+
+        BuildingData buildingData1 = new BuildingData();
+        buildingData1.setCity("New York");
+        buildingData1.setType("Skyscraper");
+        buildingData1.setCountry("United States of America");
+        buildingData1.setUse("Office Building");
+        buildingData1.setFloors(200);
+        buildingData1.setWindows(1800);
+        buildingData1.setCompletionYear(1985);
+        buildingData1.setConstructionDuration(20);
+        buildingData1.setOpen(true);
+        buildingData1.setSizeData(RoadDataInitHelper.getSizeData());
+
+        PriceData priceData1 = new PriceData();
+        priceData1.setAmount(98000000);
+        priceData1.setCurrency("US Dollar");
+        priceData1.setCode("USD");
+        priceData1.setCurrencyNumber(1);
+        priceData1.setIntroduction(123);
+
+        buildingData.setCost(priceData1);
+
+        list.add(buildingData1);
+
+        return list;
     }
 
 }
