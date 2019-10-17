7bc4c11536626e0d301e4b2d88c17eb8bf8716d5
DRIEPL <dominik.riepl@allgeier-es.com>
2019-10-17 16:37:57 +0200

    my python added

---
+++ python/dominik/data_store.py
@@ -0,0 +1,24 @@
+treeObject = {
+    "types": "cherry tree",
+    "color": "green",
+    "origin": "germany",
+    "produce": "apple",
+}
+types = ["cherry tree", "banana tree", "berry bush"]
+colors = ["red", "yellow", "green"]
+origins = ["EU", "SouthAm", "global"]
+produces = ["cherry", "banana", "berries"]
+
+def getTreeObject():
+    return treeObject
+
+def getTypes():
+    return types
+def getColors():
+    return colors
+def getOrigins():
+    return origins
+def getProduces():
+    return produces
+
+
